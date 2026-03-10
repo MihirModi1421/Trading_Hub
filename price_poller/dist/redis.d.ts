397465847a8633e6bd822b2271d3a11c1ffee7e6
MihirModi <mihirmodi1421@gmail.com>
2026-03-10 22:47:01 +0530

    remove unnecessary code

--- price_poller/dist/redis.d.ts
+++
@@ -1,8 +0,0 @@
-declare const assetArray: {
-    btc: string;
-    sol: string;
-    eth: string;
-};
-export declare function pushToRedis(redis: any, trades: any, asset: keyof typeof assetArray): Promise<void>;
-export {};
-//# sourceMappingURL=redis.d.ts.map
